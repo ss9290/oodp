2630255d8298f65f5a1fd39fa3d5f31cb5305dde
SHAIK ISMIYA <121305678+ss9290@users.noreply.github.com>
2022-12-28 19:12:09 +0530

    Create umlcode.cpp

---
+++ umlcode.cpp
@@ -0,0 +1,129 @@
+Bill_Id –
+/** 
+* Project Untitled 
+*/ 
+#ifndef _BILL_ID_H 
+#define _BILL_ID_H 
+class Bill_Id { 
+public: 
+void Bill_Id; 
+void OrderId; 
+void TotalBill; 
+void calculate_bill(); 
+}; 
+#endif //_BILL_ID_H 
+Class1 – 
+/** 
+* Project Untitled 
+*/ 
+#ifndef _CLASS1_H 
+#define _CLASS1_H 
+class Class1 { 
+}; 
+#endif //_CLASS1_H 
+6
+Customer 
+– 
+/** * Project Untitled 
+*/ 
+#ifndef 
+_CUSTOMER_H 
+#define 
+_CUSTOMER_H class 
+Customer { public: 
+void Customer Id; void 
+Customer Name; 
+void Bill Id; void Order 
+Id; void Payment Id; 
+void 
+customer_details(); 
+void ordered_items(); 
+void payment_status(); 
+}; 
+#endif //_CUSTOMER_H 
+Employees 
+–
+/** 
+* Project Untitled 
+*/ 
+#ifndef _EMPLOYEES_H 
+#define _EMPLOYEES_H 
+class Employees { 
+public: 
+7
+void EmployeeId; 
+void EmployeeName; 
+void EmployeeSalary; 
+void employee details(); 
+}; 
+#endif //_EMPLOYEES_H 
+Hotel Management System –
+/** 
+* Project Untitled 
+*/ 
+#ifndef _HOTEL MANAGEMENT 
+SYSTEM_H #define _HOTEL 
+MANAGEMENT SYSTEM_H class Hotel 
+Management System { public: 
+void Hotelname; void 
+NumberofEmployyes; 
+void open(); 
+}; 
+#endif //_HOTEL MANAGEMENT SYSTEM_H 
+Interface1 – 
+/** 
+* Project Untitled 
+*/ 
+#ifndef _INTERFACE1_H 
+#define _INTERFACE1_H 
+class Interface1 { 
+}; 
+8
+#endif //_INTERFACE1_H 
+Menu 
+– 
+/** * Project Untitled 
+*/ 
+#ifndef _MENU_H 
+#define _MENU_H 
+class Menu { 
+public: void 
+ItemId; void 
+ItemName; void 
+Amount; void 
+items(); 
+}; 
+#endif //_MENU_H 
+Order 
+–
+/** 
+* Project Untitled 
+*/ 
+#ifndef _ORDER_H 
+#define _ORDER_H 
+class Order { 
+public: void 
+OrderId; void 
+ItemId; void 
+ItemName; void 
+Quantity; void 
+9
+CustomerId; 
+void ServerId; 
+void order_items(); 
+}; 
+#endif //_ORDER_H 
+Payment 
+– 
+/** 
+* Project Untitled 
+*/ 
+#ifndef 
+_PAYMENT_H 
+#define 
+_PAYMENT_H class 
+Payment { public: 
+void PaymentId; void 
+Bill_Id; void ispaid(); 
+}; 
+#endif 
